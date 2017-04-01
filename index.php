ebfb0e4622871a35d8279baad27243d16706b047
Kevin Campo <admin@bitsngeeks.com>
2017-04-01 17:57:32 -0500

    recomomo

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+function get_client_ip_1() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+function getUserIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+if (!file_exists("newfile.txt")) {
+    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+	$txt = "DATE: ".date("Y/m/d h:i:sa").PHP_EOL;
+	$txt .= "IP: ".get_client_ip().PHP_EOL."IP2: ".get_client_ip_1().PHP_EOL."IP3: ".getUserIP().PHP_EOL."--------------------------------".PHP_EOL;
+	fwrite($myfile, $txt);
+	fclose($myfile);
+} else { 
+	$txt = "DATE: ".date("Y/m/d h:i:sa").PHP_EOL;
+	$txt .= "IP: ".get_client_ip().PHP_EOL."IP2: ".get_client_ip_1().PHP_EOL."IP3: ".getUserIP().PHP_EOL."--------------------------------".PHP_EOL;
+	file_put_contents("newfile.txt", $txt, FILE_APPEND);
+}
